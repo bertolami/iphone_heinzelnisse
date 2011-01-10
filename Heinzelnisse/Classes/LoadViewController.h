ac4912a601f63fd440dc7d43db93079f30c01518
Bertolami <roman@bertolami.net>
2011-01-10 22:42:29 +0100

    load view splash screen added

---
+++ Heinzelnisse/Classes/LoadViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoadViewController.h
+//  Heinzelnisse
+//
+//  Created by Roman Bertolami on 02.01.11.
+//  Copyright 2011 Roman Bertolami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoadViewController : UIViewController {
+
+}
+
+@end
---
+++ Heinzelnisse/Classes/LoadViewController.m
@@ -0,0 +1,22 @@
+    //
+//  LoadViewController.m
+//  Heinzelnisse
+//
+//  Created by Roman Bertolami on 02.01.11.
+//  Copyright 2011 Roman Bertolami. All rights reserved.
+//
+
+#import "LoadViewController.h"
+
+
+@implementation LoadViewController
+
+ 
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
